a955a8746d7c57ad44119ef53728bc260672056f
cam.lafit <cam.lafit@azerttyu.net>
2018-01-22 15:34:23 +0100

    Define passwd and group file
    * Take alternc account information and generate a passwd and group
    compliant files

--- src/usr/share/alternc/panel/class/m_nss.php
+++ src/usr/share/alternc/panel/class/m_nss.php
@@ -5,4 +5,36 @@
  */
 class m_nss
 {
+    protected $group_file;
+    protected $passwd_file;
+
+    public function define_files()
+    {
+        $this->define_group_file();
+        $this->define_passwd_file();
+    }
+
+    protected function define_group_file()
+    {
+        global $db;
+        $db->query("SELECT login,uid FROM `membres`");
+        $lines=array();
+        while ($db->next_record()) {
+            $lines[] = $db->f('login').":x:".$db->f('uid').":";
+        }
+
+        $this->group_file = implode("\n", $lines);
+    }
+
+    protected function define_passwd_file()
+    {
+        global $db;
+        $db->query("SELECT login,uid FROM `membres`");
+        $lines=array();
+        while ($db->next_record()) {
+            $lines[] = $db->f('login').":x:".$db->f('uid').":".$db->f('uid').":::/bin/false";
+        }
+
+        $this->passwd_file = implode("\n", $lines);
+    }
 }
