6e3f6a8a7f2f380e731950bc252c84e7e84f7f80
cam.lafit <cam.lafit@azerttyu.net>
2023-11-02 17:46:57 +0100

    Formatting document
    * Some formatting

--- src/usr/share/alternc/panel/class/m_nss.php
+++ src/usr/share/alternc/panel/class/m_nss.php
@@ -36,9 +36,9 @@ class m_nss
     {
         global $db;
         $db->query("SELECT login,uid FROM `membres`");
-        $lines=array();
+        $lines = array();
         while ($db->next_record()) {
-            $lines[] = $db->f('login').":x:".$db->f('uid').":";
+            $lines[] = $db->f('login') . ":x:" . $db->f('uid') . ":";
         }
 
         $this->group = $lines;
@@ -48,9 +48,9 @@ class m_nss
     {
         global $db;
         $db->query("SELECT login,uid FROM `membres`");
-        $lines=array();
+        $lines = array();
         while ($db->next_record()) {
-            $lines[] = $db->f('login').":x:".$db->f('uid').":".$db->f('uid')."::".getuserpath($db->f('login')).":/bin/false";
+            $lines[] = $db->f('login') . ":x:" . $db->f('uid') . ":" . $db->f('uid') . "::" . getuserpath($db->f('login')) . ":/bin/false";
         }
 
         $this->passwd = $lines;
@@ -60,19 +60,19 @@ class m_nss
     {
         global $db;
         $db->query("SELECT login FROM `membres`");
-        $lines=array();
+        $lines = array();
         while ($db->next_record()) {
-	    // shadow fields (9) :
-	    // 1. login
-	    // 2. encrypted password or * to prevent login
-	    // 3. date of last password change or '' meaning that password aging features are disabled
-	    // 4. minimum password age or '' or 0 meaning no minimum age
-	    // 5. maximum password age or '' meaning no maximum password age, no password warning period, and no password inactivity period
-	    // 6. password warning period or '' or 0 meaning there are no password warning period
-	    // 7. password inactivity period or '' for no enforcement
-	    // 8. account expiration date or '' for no expiration
-	    // 9. reserved
-	    $fields = array($db->f('login'), '*', '', '', '', '', '', '', '');
+            // shadow fields (9) :
+            // 1. login
+            // 2. encrypted password or * to prevent login
+            // 3. date of last password change or '' meaning that password aging features are disabled
+            // 4. minimum password age or '' or 0 meaning no minimum age
+            // 5. maximum password age or '' meaning no maximum password age, no password warning period, and no password inactivity period
+            // 6. password warning period or '' or 0 meaning there are no password warning period
+            // 7. password inactivity period or '' for no enforcement
+            // 8. account expiration date or '' for no expiration
+            // 9. reserved
+            $fields = array($db->f('login'), '*', '', '', '', '', '', '', '');
             $lines[] = implode(':', $fields);
         }
 
@@ -90,32 +90,32 @@ class m_nss
 
     protected function update_group_file()
     {
-        $file = $this->dir_extrausers."group";
-        $file_bck = $this->dir_backup."group";
+        $file = $this->dir_extrausers . "group";
+        $file_bck = $this->dir_backup . "group";
         $content_lines = false;
         if (file_exists($file)) {
             $content_lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         }
 
         if (!$content_lines) {
-            $content_lines=[];
+            $content_lines = [];
         }
         if (file_exists($file_bck)) {
             $content_lines_bck = file($file_bck, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            $content_lines = array_diff($content_lines,$content_lines_bck);
+            $content_lines = array_diff($content_lines, $content_lines_bck);
         }
-        $content_lines = array_merge($content_lines,$this->group);
-        $content = implode("\n",$content_lines);
-        $content_bck = implode("\n",$this->group);
+        $content_lines = array_merge($content_lines, $this->group);
+        $content = implode("\n", $content_lines);
+        $content_bck = implode("\n", $this->group);
 
-        $this->write_file($file_bck,$content_bck);
+        $this->write_file($file_bck, $content_bck);
         return $this->write_file($file, $content);
     }
 
     protected function update_passwd_file()
     {
-        $file = $this->dir_extrausers."passwd";
-        $file_bck = $this->dir_backup."passwd";
+        $file = $this->dir_extrausers . "passwd";
+        $file_bck = $this->dir_backup . "passwd";
         $content_lines = false;
         if (file_exists($file)) {
             $content_lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -126,20 +126,20 @@ class m_nss
         }
         if (file_exists($file_bck)) {
             $content_lines_bck = file($file_bck, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            $content_lines = array_diff($content_lines,$content_lines_bck);
+            $content_lines = array_diff($content_lines, $content_lines_bck);
         }
-        $content_lines = array_merge($content_lines,$this->passwd);
-        $content = implode("\n",$content_lines);
-        $content_bck = implode("\n",$this->passwd);
+        $content_lines = array_merge($content_lines, $this->passwd);
+        $content = implode("\n", $content_lines);
+        $content_bck = implode("\n", $this->passwd);
 
-        $this->write_file($file_bck,$content_bck);
+        $this->write_file($file_bck, $content_bck);
         return $this->write_file($file, $content);
     }
 
     protected function update_shadow_file()
     {
-        $file = $this->dir_extrausers."shadow";
-        $file_bck = $this->dir_backup."shadow";
+        $file = $this->dir_extrausers . "shadow";
+        $file_bck = $this->dir_backup . "shadow";
         $content_lines = false;
         if (file_exists($file)) {
             $content_lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -150,20 +150,20 @@ class m_nss
         }
         if (file_exists($file_bck)) {
             $content_lines_bck = file($file_bck, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            $content_lines = array_diff($content_lines,$content_lines_bck);
+            $content_lines = array_diff($content_lines, $content_lines_bck);
         }
-        $content_lines = array_merge($content_lines,$this->shadow);
-        $content = implode("\n",$content_lines);
-        $content_bck = implode("\n",$this->shadow);
+        $content_lines = array_merge($content_lines, $this->shadow);
+        $content = implode("\n", $content_lines);
+        $content_bck = implode("\n", $this->shadow);
 
-        $this->write_file($file_bck,$content_bck);
+        $this->write_file($file_bck, $content_bck);
         return $this->write_file($file, $content);
     }
 
-    protected function write_file($file,$content,$separator = "\n") {
-
+    protected function write_file($file, $content, $separator = "\n")
+    {
         if (is_array($content)) {
-            $content = implode($separator,$content);
+            $content = implode($separator, $content);
         }
         return file_put_contents($file, $content, LOCK_EX);
     }
@@ -174,4 +174,3 @@ class m_nss
         return true;
     }
 }
-
