461bc985928a25a5caead51455e1dc893602b2c1
cam.lafit <cam.lafit@azerttyu.net>
2024-12-03 13:37:30 +0100

    Set a prefix to login to nss context
    * As asked by #5 a prefix is set nss account to prevent conflict with system posix account

--- src/usr/share/alternc/panel/class/m_nss.php
+++ src/usr/share/alternc/panel/class/m_nss.php
@@ -9,10 +9,12 @@ class m_nss
     protected $passwd_file;
     protected $shadow_file;
     protected $login_shell_default;
+    protected $login_prefix_default;
 
     public function __construct()
     {
         $this->login_shell_default = !empty($GLOBALS['L_LOGIN_SHELL']) ? $GLOBALS['L_LOGIN_SHELL'] : "/bin/false";
+        $this->login_prefix_default = !empty($GLOBALS['L_LOGIN_PREFIX']) ? $GLOBALS['L_LOGIN_PREFIX'] . "_" : "";
     }
 
 
@@ -42,7 +44,7 @@ class m_nss
         $db->query("SELECT login,uid FROM `membres`");
         $lines = [];
         while ($db->next_record()) {
-            $lines[] = $db->f('login') . ":x:" . $db->f('uid') . ":";
+            $lines[] = $this->login_prefix_default . $db->f('login') . ":x:" . $db->f('uid') . ":";
         }
 
         $this->group_file = implode("\n", $lines);
@@ -61,7 +63,7 @@ class m_nss
         $db->query("SELECT login,uid FROM `membres`");
         $lines = [];
         while ($db->next_record()) {
-            $lines[] = $db->f('login') . ":x:" . $db->f('uid') . ":" . $db->f('uid') . "::" . getuserpath($db->f('login')) . ":" . $login_shell;
+            $lines[] = $this->login_prefix_default . $db->f('login') . ":x:" . $db->f('uid') . ":" . $db->f('uid') . "::" . getuserpath($db->f('login')) . ":" . $login_shell;
         }
 
         $this->passwd_file = implode("\n", $lines);
@@ -83,7 +85,7 @@ class m_nss
             // 7. password inactivity period or '' for no enforcement
             // 8. account expiration date or '' for no expiration
             // 9. reserved
-            $fields = [$db->f('login'), '*', '', '', '', '', '', '', ''];
+            $fields = [$this->login_prefix_default . $db->f('login'), '*', '', '', '', '', '', '', ''];
             $lines[] = implode(':', $fields);
         }
 
